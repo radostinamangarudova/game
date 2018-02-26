896204ab9bf72463e2ab1144df15f44420fea4cc
r.mangarudova <r.mangarudova@ad.mentormate.bg>
2018-02-26 19:23:34 +0200

    Set rules

--- game.php
+++ game.php
@@ -5,23 +5,26 @@ function play() {
     $user_choice = $_GET['choice'];
     if($user_choice == true) :
         validate_choice($user_choice);
-        echo $_GET['choice'];
     else:
-        display();
+        display($user_choice);
     endif;
 }
 
 //Display all the choices in the game dynamically. (it could be values from a database.)
-function display() {
+function display($choice) {
     $choices = [
         "rock" => '<a href="?choice=rock"><img src="images/rock.png" alt="rock" name="rock"></a>',
         "paper" => '<a href="?choice=paper"><img src="images/paper.png" alt="paper" name="paper"></a>',
         "scissors" => ' <a href="?choice=scissors"><img src="images/scissors.png" alt="scissors" name="scissors"></a>'
     ];
-                
-    foreach($choices as $choice=>$value) :
-        echo $value;
-    endforeach;
+    
+    if ($choice == null) :            
+        foreach($choices as $choice=>$value) :
+            echo $value;
+        endforeach;
+    else :
+        echo $choices[$choice];
+    endif;
 }
 
 //Prevent user from typing a random word in the url
@@ -32,7 +35,25 @@ function validate_choice($choice){
         echo '<h1 style="color:red;">Invalid.</h1>';
         die;
     else :
-        echo $choice;
+        play_round($choice);
     endif;
+}
+
+function play_round($choice){
+    $available_choices = ['rock', 'paper', 'scissors'];
+    $generated = $available_choices[rand(0, 2)];
+    if($choice == 'scissors' && $generated == 'paper' || 
+        $choice == 'paper' && $generated == 'rock' ||
+        $choice == 'rock' && $generated == 'scissors') :
+        echo '<h1 style="color:green;">You Won! :)</h1>';
+    elseif ($choice == $generated) : {
+        echo '<h1 style="color:blue;">Standoff. :|</h1>';
+    }
+    else :
+        echo '<h1 style="color:red;">You lost! :(</h1>';
+    endif;
+    
+    display($choice);
+    display($generated);
 }
 ?>
